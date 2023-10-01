e1cae6c63f1dbb4a97d4f35ee7904e99466c7251
Anvar <anver12488@gmail.com>
2023-10-01 12:52:26 +0400

    hw

---
+++ 21.09.2023.cs
@@ -0,0 +1,59 @@
+﻿// public delegate void Operation(double amount);
+// public class Bankacc
+// {
+//     private double balance;
+//     private string name;
+//     public Bankacc(string name, double balance)
+//     {
+//         this.name = name;
+//         this.balance = balance;
+//     }
+//     public void Deposit(double amount)
+//     {
+//         balance += amount;
+//         Console.WriteLine($"Deposited: {amount:C}. New balance: {balance:C}");
+//         AccOperation(amount, "Deposit");
+//     }
+//     public void Withdraw(double amount)
+//     {
+//         if (balance >= amount)
+//         {
+//             balance -= amount;
+//             Console.WriteLine($"Withdrawn: {amount:C}. New balance: {balance:C}");
+//             AccOperation(amount, "Withdrawal");
+//         }
+//         else
+//         {
+//             Console.WriteLine("Insufficient funds. Withdrawal failed.");
+//         }
+//     }
+//
+//     public void PrintBalance()
+//     {
+//         Console.WriteLine($"Current balance for account '{name}': {balance:C}");
+//     }
+//
+//     public event Operation AccOperation;
+//
+// }
+//
+// class Program
+// {
+//     static void Main()
+//     {
+//         Bankacc account = new Bankacc("Anver's account", 1000);
+//
+//         account.AccOperation += LogOperation;
+//
+//         account.Deposit(500);
+//         account.Withdraw(200);
+//         account.Withdraw(1000); 
+//
+//         account.PrintBalance();
+//     }
+//
+//     static void LogOperation(double amount)
+//     {
+//         Console.WriteLine($"Logged operation. Amount: {amount:C}");
+//     }
+// }
